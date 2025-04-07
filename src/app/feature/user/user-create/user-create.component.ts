2cc98cc7955a353fc2dd362976441cf52a784cdb
adrian-johnson <4corderoj@gmail.com>
2025-04-07 16:35:19 -0400

    finished user create - user edit I think is done too? and working on user detail

--- src/app/feature/user/user-create/user-create.component.ts
+++ src/app/feature/user/user-create/user-create.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { User } from '../../../model/user';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+import { UserService } from '../../../service/user.service';
 
 @Component({
   selector: 'app-user-create',
   standalone: false,
   templateUrl: './user-create.component.html',
-  styleUrl: './user-create.component.css'
+  styleUrl: './user-create.component.css',
 })
-export class UserCreateComponent {
+export class UserCreateComponent implements OnInit, OnDestroy {
+  title: string = 'User-Create';
+  newUser: User = new User();
+  subscription!: Subscription;
+  jobs: string[] = [
+    'Paladin',
+    'Mageblade',
+    'Death Knight',
+    'Cleric',
+    'Hunter',
+    'Smuggler',
+    'Warrior',
+  ];
 
+  constructor(private userSvc: UserService, private router: Router) {}
+
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  addUser(): void {
+    this.subscription = this.userSvc.add(this.newUser).subscribe((resp) => {
+      this.router.navigateByUrl('/user-list');
+    });
+  }
 }
--- src/app/feature/user/user-detail/user-detail.component.ts
+++ src/app/feature/user/user-detail/user-detail.component.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { User } from '../../../model/user';
+import { Subscription } from 'rxjs';
+import { UserService } from '../../../service/user.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-detail',
   standalone: false,
   templateUrl: './user-detail.component.html',
-  styleUrl: './user-detail.component.css'
+  styleUrl: './user-detail.component.css',
 })
-export class UserDetailComponent {
+export class UserDetailComponent implements OnInit, OnDestroy {
+  title: string = 'Movie-Detail';
+  userId!: number;
+  user!: User;
+  subscription!: Subscription;
+  jobs: string[] = [
+    'Paladin',
+    'Mageblade',
+    'Death Knight',
+    'Cleric',
+    'Hunter',
+    'Smuggler',
+    'Warrior',
+  ];
 
+  constructor(
+    private userSvc: UserService,
+    private router: Router,
+    private actRoute: ActivatedRoute
+  ) {}
+  ngOnInit(): void {
+    this.actRoute.params.subscribe((parms) => {
+      this.userId = parms['id'];
+      this.subscription = this.userSvc.getById(this.userId).subscribe({
+        next: (resp) => {
+          this.user = resp;
+        },
+        error: (err) => {
+          console.log('Error retirieving user: ', err);
+        },
+      });
+    });
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  delete() {
+    this.userSvc.delete(this.userId).subscribe({
+      next: (resp) => {
+        this.router.navigateByUrl('/user-list');
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
